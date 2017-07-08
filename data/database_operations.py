2187990f49e9350e51178ab277786af91918108b
Ian Whitestone <ianjameswhitestone@gmail.com>
2017-07-08 13:48:33 -0400

    postgres copy functionality

--- data/database_operations.py
+++ data/database_operations.py
@@ -20,6 +20,72 @@ CREATE TABLE tickets (
 CREATE INDEX tickets_loc ON tickets (location2);
 """
 
+
+class IteratorFile(io.TextIOBase):
+    """ given an iterator which yields strings,
+    return a file like object for reading those strings.
+    Taken from https://gist.github.com/jsheedy/ed81cdf18190183b3b7d
+    """
+
+    def __init__(self, it):
+        self._it = it
+        self._f = io.StringIO()
+
+    def read(self, length=sys.maxsize):
+
+        try:
+            while self._f.tell() < length:
+                self._f.write(
+                    unicode(next(self._it), 'utf-8').encode('utf-8') + u"\n")
+
+        except StopIteration as e:
+            # soak up StopIteration. this block is not necessary because
+            # of finally, but just to be explicit
+            pass
+
+        except Exception as e:
+            print("uncaught exception: {}".format(e))
+
+        finally:
+            self._f.seek(0)
+            data = self._f.read(length)
+
+            # save the remainder for next read
+            remainder = self._f.read()
+            self._f.seek(0)
+            self._f.truncate(0)
+            self._f.write(remainder)
+            return data
+
+    def readline(self):
+        return next(self._it)
+
+def postgres_load(conn, tablename, data):
+    """
+    Function to load data into a Postgres table
+    ------
+    params
+        conn <Pyscopg Connection> : Connection to postgres data base
+        data <list> : list of tuples, where each row is a tuple
+        tablename <string> : tablename to load data into
+    """
+    try:
+        log.info('Attempting to load data into Postgres table %s' % tablename)
+        curs = conn.cursor()
+        table_width = len(data[0])
+        template_string = "|".join(['{}'] * table_width)
+        f = IteratorFile((template_string.format(*x) for x in data))
+        results = curs.copy_from(f, tablename, sep="|", null='NULL')
+        curs.close()
+        conn.commit()
+        log.info('Data succesfully loaded')
+        return True
+    except Exception as e:
+        log.exception("Unable to load data to Postgres due to error: %s"
+                        % e.message)
+        return False
+
+
 def getConnection():
     """
     Function to create a connection to postgres.
