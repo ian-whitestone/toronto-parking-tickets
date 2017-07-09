9c0f20d753f342cf7a843d365eb6e701d52a96be
iwhitest <ianjameswhitestone@gmail.com>
2017-07-09 13:19:36 -0400

    ETL process

--- data/config.py
+++ data/config.py
@@ -1,4 +1,20 @@
-# Postgres DB details
-PS_HOST_NAME = "localhost"
-PS_DB_NAME = "toronto"
+from private import *
+
+## POSTGRES CONECTION DETAILS
+PS_HOST_NAME = "toronto.cfeeqz1xpx0l.us-west-2.rds.amazonaws.com"
+PS_DB_NAME = "parking"
 PS_PORT = 5432
+
+
+# DB COLS
+
+FIELD_MAP = [
+    {'name':'date', 'func':'do_none'},
+    {'name':'time_of_infraction', 'func':'check_int'},
+    {'name':'infraction_code', 'func':'check_int'},
+    {'name':'infraction_description', 'func':'check_text'},
+    {'name':'set_fine_amount', 'func':'check_int'},
+    {'name':'location1', 'func':'check_varchar', 'length': 10},
+    {'name':'location2', 'func':'check_varchar', 'length': 50},
+    {'name':'province', 'func':'check_varchar', 'length': 5}
+]
